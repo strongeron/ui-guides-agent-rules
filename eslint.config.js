c69c011be49d434e9f9bf653d05fc3e655d40850
Gleb Stroganov <strongeron@gmail.com>
2026-01-20 21:27:18 +0000

    Fix lint and typecheck blockers

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,13 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+
+const a11yOff = Object.fromEntries(
+  Object.keys(jsxA11y.configs.recommended.rules).map((rule) => [rule, 'off'])
+);
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -14,15 +19,31 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      'jsx-a11y': jsxA11y,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...jsxA11y.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    files: ['src/components/examples/**/*.{ts,tsx}'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off',
+    },
+    rules: a11yOff,
+  },
+  {
+    files: ['src/mdx-components.tsx'],
+    rules: {
+      'jsx-a11y/anchor-has-content': 'off',
+      'jsx-a11y/heading-has-content': 'off',
     },
   }
 );
